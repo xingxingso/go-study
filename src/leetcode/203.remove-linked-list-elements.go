52b315d99697bcb582303d5329496940d9818b19
Kant Chan <1126973789@qq.com>
2019-08-07 15:14:34 +0800

    Update

---
+++ src/leetcode/203.remove-linked-list-elements.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+    for ListNode != null && ListNode.Next != null {
+        if ListNode.Next.Val == val {
+            ListNode.Next = ListNode.
+        }
+    } 
+}
+
+func main() {
+	var node ListNode{}
+}
